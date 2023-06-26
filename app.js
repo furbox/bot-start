ec2ad24bbf5a841782f39b4e68d31ffe651bb59e
Christopher Flores <furbox@gmail.com>
2023-06-26 12:05:03 -0600

    news

---
+++ app.js
@@ -0,0 +1,31 @@
+const {
+  createBot,
+  createProvider,
+  createFlow,
+  addKeyword,
+  EVENTS,
+} = require("@bot-whatsapp/bot");
+
+const QRPortalWeb = require("@bot-whatsapp/portal");
+const BaileysProvider = require("@bot-whatsapp/provider/baileys");
+const JsonFileAdapter = require("@bot-whatsapp/database/json");
+
+const flowPrincipal = addKeyword(EVENTS.WELCOME)
+  .addAnswer("🙌 Hola bienvenido a este *Chatbot*")
+  .addAnswer(["Hola a todos"]);
+
+const main = async () => {
+  const adapterDB = new JsonFileAdapter();
+  const adapterFlow = createFlow([flowPrincipal]);
+  const adapterProvider = createProvider(BaileysProvider);
+
+  createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
+
+  QRPortalWeb();
+};
+
+main();
